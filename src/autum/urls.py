dc8dee50226536d9cc5a22c36aea9f546bbd4382
Megha Gajbhiye <Megha Gajbhiye>
2017-02-28 17:36:29 -0800

    first commit

---
+++ src/autum/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from cloud.views import home
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    # Examples:
+    url(r'^$', home, name='home'),
+    url(r'^contact/$', 'cloud.views.contact', name='contact'),
+
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+	
