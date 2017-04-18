b12dd3ad9bfb9aed81a0fa67737cbcd2ddc76e61
MeghaGajbhiye <megha.gajbhiye@sjsu.edu>
2017-04-17 17:40:03 -0700

    updated aws.py

--- src/aws/aws.py
+++ src/aws/aws.py
@@ -1,36 +1,56 @@
 import types
+import boto3
+from datetime import datetime, timedelta
 
 class AWS:
 
-    def __init__(self, ak, sk, func=None):
+    def __init__(self, ak, sk):
         self.access_key = ak
         self.secret_key = sk
-        if func is not None:
-            self.execute = func
-
-    def execute(self):
-        print("AWS")
 
     def get_acccess_key(self):
         return self.access_key
+        #return self.access_key
 
     def get_secret_key(self):
         return self.secret_key
-
-    # def cloudwatchmonitoring:
-
+        #return self.secret_key
 
     def describe_instances(self):
-        ec2 = boto3.client('ec2', aws_access_key_id=getAccessKey(), aws_secret_access_key=getSecretKey())
+        print ('Inside describe method')
+        ec2 = boto3.client('ec2')
         ec2_out = ec2.describe_instances()
+        list_instanceid = []
         for reservation in ec2_out['Reservations']:
-            reservation_id = reservation['ReservationId']
             instance_id = reservation['Instances'][0]['InstanceId']
-            print (reservation_id, instance_id)
+            print (instance_id)
+            list_instanceid.append(instance_id)
+        return list_instanceid
+
+    def cloudwatchmonitoring(self):
+        print "I am inside cloudwatch"
+        instance_id_list  = self.describe_instances()
+        now = datetime.utcnow()
+        past = now - timedelta(minutes=30)
+        future = now + timedelta(minutes=10)
+        print ("size of instances = " + instance_id_list.len)
+        cw_client = boto3.client('CloudWatch', aws_access_key_id=self.get_acccess_key(), aws_secret_access_key=self.get_secret_key())
+        for instance_id in instance_id_list:
+            cw_out = cw_client.get_metric_statistics(
+                NameSpace='AWS/EC2',
+                MetricName='CPUUtilization',
+                Dimensions=[{'Name': 'InstanceId', 'Value': instance_id}],
+                StartTime=past,
+                EndTime=future,
+                Period=300,
+                Statistics=['Average']
+            )
+            print (cw_out)
+
 
     def launch_instance(self):
-        ec2 = boto3.client('ec2', aws_access_key_id=getAccessKey(),
-                           aws_secret_access_key=getSecretKey())
+        ec2 = boto3.client('ec2', aws_access_key_id=get_acccess_key(),
+                           aws_secret_access_key=get_secret_key())
         ec2_out = ec2.run_instances(ImageId='ami-8ca83fec', MinCount=1, MaxCount=1, KeyName='ravi',
                                     InstanceType='m4.xlarge', Monitoring={'Enabled': True})
         print (ec2_out)
@@ -39,24 +59,30 @@ class AWS:
 
 
     def describe_security_groups(self):
-        ec2 = boto3.client('ec2', aws_access_key_id=getAccessKey(),
-                           aws_secret_access_key=getSecretKey())
+        ec2 = boto3.client('ec2', aws_access_key_id=get_acccess_key(),
+                           aws_secret_access_key=get_secret_key())
         ec2_out = ec2.describe_security_groups()
         for security_group in ec2_out['SecurityGroups']:
             print (security_group['GroupName'])
 
     def describe_subnets(self):
-        ec2 = boto3.client('ec2', aws_access_key_id=getAccessKey(),
-                           aws_secret_access_key=getSecretKey())
+        ec2 = boto3.client('ec2', aws_access_key_id=get_acccess_key(),
+                           aws_secret_access_key=get_secret_key())
         ec2_out = ec2.describe_subnets()
         for subnet in ec2_out['Subnets']:
             print (subnet['SubnetId'])
 
     def describe_images(self):
-        ec2 = boto3.client('ec2', aws_access_key_id=getAccessKey(),
-                           aws_secret_access_key=getSecretKey())
+        ec2 = boto3.client('ec2', aws_access_key_id=get_acccess_key(),
+                           aws_secret_access_key=get_secret_key())
         ec2_out = ec2.describe_images()
         for image in ec2_out['Images']:
             image_id = image['ImageId']
             # image_os = image['Name']
             print (image_id, image_os)
+
+
+if __name__ == "__main__":
+    aws = AWS("AKIAJRQMACIVSVREZ6OA", "ZOcCCejDCDWLoMNhzpr0R+YJQvr0n2mvwHAhy9zy")
+    aws.cloudwatchmonitoring()
+
