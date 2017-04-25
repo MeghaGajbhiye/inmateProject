763879c64aae6e0fb138363f645a52da7dfd8182
MeghaGajbhiye <megha.gajbhiye@sjsu.edu>
2017-04-25 12:48:59 -0700

    reset to a04b470152cffa37fd499cc0cb9baf86f515117d

--- src/aws/aws.py
+++ src/aws/aws.py
@@ -1,7 +1,7 @@
 import types
 import boto3
 from datetime import datetime, timedelta
-
+ 
 class AWS:
 
     def __init__(self, ak, sk):
@@ -27,35 +27,19 @@ class AWS:
             list_instanceid.append(instance_id)
         return list_instanceid
 
-    def cloudwatchmonitoring(self):
-        print "I am inside cloudwatch"
-        instance_id_list  = self.describe_instances()
-        now = datetime.utcnow()
-        past = now - timedelta(minutes=30)
-        future = now + timedelta(minutes=10)
-        print ("size of instances = " + instance_id_list.len)
-        cw_client = boto3.client('CloudWatch', aws_access_key_id=self.get_acccess_key(), aws_secret_access_key=self.get_secret_key())
-        for instance_id in instance_id_list:
-            cw_out = cw_client.get_metric_statistics(
-                NameSpace='AWS/EC2',
-                MetricName='CPUUtilization',
-                Dimensions=[{'Name': 'InstanceId', 'Value': instance_id}],
-                StartTime=past,
-                EndTime=future,
-                Period=300,
-                Statistics=['Average']
-            )
-            print (cw_out)
+    def launch_instance(self, min_count, max_count, key_name, instance_type,monitoring):
+        print self.access_key, self.secret_key, min_count, max_count,key_name,instance_type,monitoring
 
-
-    def launch_instance(self):
+        print "I am inside launch instance"
         ec2 = boto3.client('ec2', aws_access_key_id=self.get_acccess_key(),
                            aws_secret_access_key=self.get_secret_key())
         image_id = self.describe_images()
         # ec2_out = ec2.run_instances(ImageId='ami-8ca83fec', MinCount=1, MaxCount=1, KeyName='ravi',
         #                             InstanceType='m4.xlarge', Monitoring={'Enabled': True})
-        ec2_out = ec2.run_instances(ImageId='ami-8ca83fec', MinCount=1, MaxCount=1, KeyName='ravi',
-                                    InstanceType='m4.xlarge', Monitoring={'Enabled': True})
+
+
+        ec2_out = ec2.run_instances(ImageId=image_id, MinCount=min_count, MaxCount=max_count, KeyName=key_name,
+                                    InstanceType=instance_type, Monitoring={'Enabled': monitoring})
         # print (ec2_out)
 
     # http://boto3.readthedocs.io/en/latest/reference/services/ec2.html#EC2.Client.run_instances
@@ -84,9 +68,38 @@ class AWS:
             return image_id
             # print (image_id, image_os)
 
+    def terminate_instance(self, instance_id):
+
+        ec2 = boto3.client ('ec2', aws_access_key_id=self.get_acccess_key(),
+                            aws_secret_access_key=self.get_secret_key())
+        ec2_out = ec2.terminate_instances (InstanceIds=[instance_id])
+        print (ec2_out)
+
+    # def cloudwatchmonitoring(self):
+    #     print "I am inside cloudwatch"
+    #     instance_id_list  = self.describe_instances()
+    #     now = datetime.utcnow()
+    #     past = now - timedelta(minutes=30)
+    #     future = now + timedelta(minutes=10)
+    #     print ("size of instances = " + instance_id_list.len)
+    #     cw_client = boto3.client('CloudWatch', aws_access_key_id=self.get_acccess_key(), aws_secret_access_key=self.get_secret_key())
+    #     for instance_id in instance_id_list:
+    #         cw_out = cw_client.get_metric_statistics(
+    #             NameSpace='AWS/EC2',
+    #             MetricName='CPUUtilization',
+    #             Dimensions=[{'Name': 'InstanceId', 'Value': instance_id}],
+    #             StartTime=past,
+    #             EndTime=future,
+    #             Period=300,
+    #             Statistics=['Average']
+    #         )
+    #         print (cw_out)
+
+
+
 
 if __name__ == "__main__":
-    # aws_access_key_id = '', aws_secret_access_key = ''
-    aws = AWS("", "")
+    # aws_access_key_id = '', aws_secret_access_key = 'ZOcCCejDCDWLoMNhzpr0R+YJQvr0n2mvwHAhy9zy'
+    aws = AWS("AKIAJRQMACIVSVREZ6OA", "ZOcCCejDCDWLoMNhzpr0R+YJQvr0n2mvwHAhy9zy")
     aws.describe_instances()
 
