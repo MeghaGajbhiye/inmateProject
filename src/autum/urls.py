d2ec907aef2d0b4333f43fa696d0449c89975a73
Seizon-sha <shaseizon@gmail.com>
2017-05-01 20:36:37 -0700

    azure delete

--- src/autum/urls.py
+++ src/autum/urls.py
@@ -5,13 +5,11 @@ from django.conf import settings
 from cloud.views import home, contact, dashboard, about, acknowledgement, support, migration, monitor, cpfinal, instance
 
 from aws.views import aws, aws_home, aws_delete, aws_create
-from azure.views import azure, azure_home, azure_create, azure_update, azure_delete_vm, azure_start, azure_stop, \
-    azure_reboot, azure_view
+from azure.views import azure, azure_home, azure_create, azure_update, azure_delete, azure_delete_vm, azure_delete_rsgrp, azure_start, azure_stop, azure_reboot, azure_view
 from ibm.views import ibm, ibm_home
 
 from google.views import google, google_create, google_retrieve, google_delete, google_home
-from rackspace.views import rackspace, rackspace_create, rackspace_home, rackspace_update, rackspace_delete, \
-    rackspace_reboot, rackspace_view
+from rackspace.views import rackspace, rackspace_create, rackspace_home, rackspace_update, rackspace_delete, rackspace_reboot, rackspace_view
 
 urlpatterns = [
     # Examples:
@@ -32,7 +30,9 @@ urlpatterns = [
     url(r'^azure_reboot/$', azure_reboot, name='azure_reboot'),
     url(r'^azure_create/$', azure_create, name='azure_create'),
     url(r'^azure_update/$', azure_update, name='azure_update'),
+    url(r'^azure_delete/$', azure_delete, name='azure_delete'),
     url(r'^azure_delete_vm/$', azure_delete_vm, name='azure_delete_vm'),
+    url(r'^azure_delete_rsgrp/$', azure_delete_rsgrp, name='azure_delete_rsgrp'),
     url(r'^azure_view/$', azure_view, name='azure_view'),
     url(r'^ibm/$', ibm, name='ibm'),
     url(r'^IBM_Home/$', ibm_home, name='ibm_home'),
