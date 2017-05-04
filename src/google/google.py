9dcf2ef5923d10cbe37d43994f8050a87ad264bc
MeghaGajbhiye <megha.gajbhiye@sjsu.edu>
2017-05-04 14:29:48 -0700

    configured google.py file + tests for google and rackspace

--- src/google/google.py
+++ src/google/google.py
@@ -28,7 +28,6 @@ class Google:
         google_access_token= auth_response['access_token']
         google_credentials = AccessTokenCredentials(google_access_token, "MyAgent/1.0", None)
         self.compute = build('compute', 'v1', credentials=google_credentials)
-        # credentials = GoogleCredentials.get_application_default ()
 
     def operation_wait(self, project_id, google_zone, operation_name):
         print('Waiting for operation to finish...')
@@ -49,13 +48,13 @@ class Google:
     def create_instance(self, project_id, zone, instance_name, bucket_name ):
 
         print('Creating instance.')
-        # Get the latest Debian Jessie image.
-        # compute = googleapiclient.discovery.build('compute', 'v1')
+
+        #getting latest debian-8 image.
         image_result = self.compute.images().getFromFamily(
             project='debian-cloud', family='debian-8').execute()
         disk_image = image_result['selfLink']
 
-        # Configuring the virtual machine
+        # Configuring the virtual machine with machine type, startup script, and image configurations.
         machine_type = "zones/%s/machineTypes/n1-standard-1" % zone
         google_startup_script = open(
             os.path.join(
@@ -67,7 +66,7 @@ class Google:
             'name': instance_name,
             'machineType': machine_type,
 
-            # Specify the boot disk and the image to use as a source.
+            # Configuring boot disk.
             'disks': [
                 {
                     'boot': True,
@@ -78,8 +77,7 @@ class Google:
                 }
             ],
 
-            # Specify a network interface with NAT to access the public
-            # internet.
+            # COnfiguring network interface
             'networkInterfaces': [{
                 'network': 'global/networks/default',
                 'accessConfigs': [
@@ -87,7 +85,7 @@ class Google:
                 ]
             }],
 
-            # Allow the instance to access cloud storage and logging.
+            # COnfiguring scope.
             'serviceAccounts': [{
                 'email': 'default',
                 'scopes': [
@@ -96,12 +94,9 @@ class Google:
                 ]
             }],
 
-            # Metadata is readable from the instance and allows you to
-            # pass configuration from deployment scripts to instances.
+            # configuring metadata
             'metadata': {
                 'items': [{
-                    # Startup script is automatically executed by the
-                    # instance upon startup.
                     'key': 'startup-script',
                     'value': google_startup_script
                 }, {
@@ -124,11 +119,6 @@ class Google:
 
         self.operation_wait(project_id, zone, operation['name'])
 
-        print("""
-           Instance created.
-           It will take a minute or two for the instance to complete work.
-           """)
-
 
     def list_instances(self, project_id, zone):
         # compute = googleapiclient.discovery.build('compute', 'v1')
@@ -142,42 +132,32 @@ class Google:
     def delete_instance(self, project_id, zone, instance_name):
 
         print('Deleting instance.')
-        # compute = googleapiclient.discovery.build('compute', 'v1')
-
-        # operation = self.delete_instance(compute, self.get_project_id(), self.get_zone(), self.get_instance_name())
-
         operation = self.compute.instances().delete(project=project_id, zone=zone, instance=instance_name).execute()
         self.operation_wait(project_id, zone, operation['name'])
 
-        print("""  Instance Deleted.
-                   It will take a minute or two for the instance to be deleted.
-                   """)
 
     def format_rfc3339(self, datetime_instance=None):
-        """Formats a datetime per RFC 3339.
-        :param datetime_instance: Datetime instanec to format, defaults to utcnow
-        """
         return datetime_instance.isoformat ("T") + "Z"
 
     def get_now_rfc3339(self):
         return self.format_rfc3339 (datetime.datetime.utcnow ())
 
     def cloud_monitor(self, project_id):
-        CUSTOM_METRIC_NAME = "custom.cloudmonitoring.googleapis.com/pid"
-        client = googleapiclient.discovery.build ('cloudmonitoring', 'v2beta2')
-        now = self.get_now_rfc3339 ()
-        desc = {"project": project_id,
-                "metric": CUSTOM_METRIC_NAME}
-        point = {"start": now,
-                 "end": now,
+        GOOGLE_METRIC_NAME = "custom.cloudmonitoring.googleapis.com/pid"
+        google_client = googleapiclient.discovery.build ('cloudmonitoring', 'v2beta2')
+        set_now = self.get_now_rfc3339 ()
+        description = {"project": project_id,
+                "metric": GOOGLE_METRIC_NAME}
+        point = {"start": set_now,
+                 "end": set_now,
                  "doubleValue": os.getpid ()}
-        print("Writing {} at {}".format (point["doubleValue"], now))
+        print("Writing {} at {}".format (point["doubleValue"], set_now))
 
         # Write a new data point.
         try:
-            write_request = client.timeseries ().write (
+            write_request = google_client.timeseries ().write (
                 project=project_id,
-                body={"timeseries": [{"timeseriesDesc": desc, "point": point}]})
+                body={"timeseries": [{"timeseriesDesc": description, "point": point}]})
             write_request.execute ()  # Ignore the response.
         except Exception as e:
             print("Failed to write custom metric data: exception={}".format (e))
@@ -187,10 +167,10 @@ class Google:
         # When a custom metric is created, it may take a few seconds
         # to propagate throughout the system. Retry a few times.
         print("Reading data from custom metric timeseries...")
-        read_request = client.timeseries ().list (
+        read_request = google_client.timeseries ().list (
             project=project_id,
-            metric=CUSTOM_METRIC_NAME,
-            youngest=now)
+            metric=GOOGLE_METRIC_NAME,
+            youngest=set_now)
         start = time.time ()
         while True:
             try:
@@ -223,5 +203,5 @@ if __name__ == '__main__':
     # gc.set_zone("us-central1-f")
     # gc.set_instance_name("autumtest2")
     gc.create_instance("autum-165318","us-central1-f","autumtest-2","autum2" )
-    gc.delete_instance("autum-165318", "us-central1-f", "autumtest2")
+    # gc.delete_instance("autum-165318", "us-central1-f", "autumtest2")
     # gc.cloud_monitor("autum-165318")
