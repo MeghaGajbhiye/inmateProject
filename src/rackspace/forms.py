27fcdf9815644aff333d27a16e9d250c8fc42168
Seizon-sha <shaseizon@gmail.com>
2017-05-07 16:26:24 -0700

    added new

--- src/rackspace/forms.py
+++ src/rackspace/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from .models import Rackspace
 
 class RackspaceForm(forms.ModelForm):
+
+	def __init__(self, *args, **kwargs):
+		super(RackspaceForm, self).__init__(*args, **kwargs)
+		self.fields['username'].help_text = '<br/>Enter username of Rackspace'
+
 	class Meta:
 		model = Rackspace
-		fields = ['tenant_id']
+		fields = ['username', 'api_key']
+
+
--- src/rackspace/models.py
+++ src/rackspace/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Rackspace(models.Model):
-	tenant_id = models.CharField(max_length= 120, blank = True, null = True)
+	username = models.CharField(max_length= 120, blank = True, null = True)
+	api_key = models.CharField(max_length= 120, blank = True, null = True)
 	# class Meta:
 	# 	# managed = False
 	# 	db_table = 'rackspace'
