f05c51739c0782f1e8a64da4afea165108cfbc10
Seizon-sha <shaseizon@gmail.com>
2017-05-10 15:32:44 -0700

    new - 3:32pm

--- src/autum/urls.py
+++ src/autum/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from cloud.views import home, contact, dashboard, about, acknowledgement, support, migration, monitor, cpfinal, instance
 
-from aws.views import aws, aws_cp, aws_home, aws_delete, aws_create, aws_monitor, aws_view, aws_view_delete, aws_monitor_list
+from aws.views import aws, aws_cp, aws_home, aws_delete, aws_create, aws_monitor, aws_view, aws_monitor_list
 from azure1.views import azure_cp, azure_cp1, azure_home, azure_create, azure_update, azure_delete, azure_delete_vm, azure_delete_rsgrp, azure_start, azure_stop, azure_reboot, azure_view, azure_monitor, sub, res
 from ibm.views import ibm, ibm_home
 
@@ -24,7 +24,7 @@ urlpatterns = [
     url(r'^aws_home/$', aws_home, name='aws_home'),
     url(r'^aws_delete/$', aws_delete, name='aws_delete'),
     url(r'^aws_view/$', aws_view, name='aws_view'),
-    url(r'^aws_view_delete/$', aws_view_delete, name='aws_view_delete'),
+    # url(r'^aws_view_delete/$', aws_view_delete, name='aws_view_delete'),
     # url(r'^aws_view_list/$', aws_view_list, name='aws_view_list'),
     url(r'^aws_monitor/$', aws_monitor, name='aws_monitor'),
     url(r'^azure/$', azure_cp, name='azure_cp'),
